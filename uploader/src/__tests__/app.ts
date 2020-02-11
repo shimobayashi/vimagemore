4c8596c3985f5279c7497a7a344c8f3952e92ffd
shimobayashi <akimasashimobayashi@gmail.com>
2020-02-11 21:35:17 +0900

    とりあえずテストを書きつつS3にアップロードする処理を書いてみたが実際には実行してないので動くのかどうか不明。LambdaにS3へのアクセス権限も必要そう。

--- uploader/src/__tests__/app.ts
+++ uploader/src/__tests__/app.ts
@@ -1,18 +1,45 @@
+import AWS from 'aws-sdk';
+
 import * as app from '../app';
-var event:any, context:any;
 
+// 参考: https://github.com/aws/aws-sdk-js/issues/1963#issuecomment-508210959
+// 参考: https://jestjs.io/docs/ja/tutorial-async
+jest.mock('aws-sdk', () => {
+    return {
+        S3: () => {
+            return {
+                putObject: () => {
+                    return {
+                        promise: () => {
+                            return new Promise((resolve) => {
+                                resolve();
+                            })
+                        },
+                    }
+                },
+            }
+        },
+    };
+});
 describe('Tests index', () => {
-    test('verifies successful response', async () => {
-        const result = await app.lambdaHandler(event, context)
+    test('verifies successful response', done => {
+        const callback:Function = (err:any, result:any) => {
+            try {
+                expect(result).toEqual(expect.any(Object));
+                expect(result.statusCode).toBe(200);
+                expect(result.body).toEqual(expect.any(String));
 
-        expect(result).toEqual(expect.any(Object));
-        expect(result.statusCode).toBe(200);
-        expect(result.body).toEqual(expect.any(String));
+                let response = JSON.parse(result.body);
 
-        let response = JSON.parse(result.body);
+                expect(response).toEqual(expect.any(Object));
+                expect(response.message).toMatch("Succeed!");
 
-        expect(response).toEqual(expect.any(Object));
-        expect(response.message).toMatch("hello world");
-        // expect(response.location).to.be.an("string");
+                done();
+            } catch (error) {
+                done(error);
+            }
+        };
+
+        app.lambdaHandler({}, {}, callback);
     });
 });
--- uploader/src/app.ts
+++ uploader/src/app.ts
@@ -1,8 +1,36 @@
-export async function lambdaHandler (event:any, context:any) {
-    return  {
-        'statusCode': 200,
-        'body': JSON.stringify({
-            message: 'hello world',
-        })
-    }
+import AWS from 'aws-sdk';
+
+export async function lambdaHandler (event:any, context:any, callback:Function) {
+    /* 画像データをS3へ配置する */
+    // 参考: https://docs.aws.amazon.com/ja_jp/sdk-for-javascript/v2/developer-guide/using-promises.html
+    // 参考: https://qiita.com/niusounds/items/9be50e9d8538db052275
+    const s3 = new AWS.S3();
+    const params:AWS.S3.Types.PutObjectRequest = {
+        Bucket: process.env.VIMAGEMORE_BUCKET_NAME || '',
+        Key: 'image/test.txt', //XXX なんかいい感じに決める
+        ContentType: 'text/plain', //XXX
+        Body: 'this is test', //XXX busboyを使って画像を読み込む
+    };
+    s3.putObject(params).promise().then((data) => {
+        console.log('Success', data);
+
+        /* メタデータをDynamoDBへ記録する */
+        //XXX
+
+        callback(null, {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'Succeed!',
+            })
+        });
+    }).catch((err) => {
+        console.log('Error', err);
+
+        callback(err, {
+            'statusCode': 500,
+            'body': JSON.stringify({
+                message: err,
+            })
+        });
+    })
 }
