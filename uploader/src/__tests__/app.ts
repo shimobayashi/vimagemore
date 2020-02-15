f9f91566710d21949d60272dc82b6df90145b39b
shimobayashi <akimasashimobayashi@gmail.com>
2020-02-15 20:30:20 +0900

    画像を実際にアップロードしそうな感じにガーッと書いてみた。テストしかしてないので本番で動くのか不明。

--- uploader/src/__tests__/app.ts
+++ uploader/src/__tests__/app.ts
@@ -1,30 +1,51 @@
 import AWS from 'aws-sdk';
+import fs from 'fs';
 
 import * as app from '../app';
 
 // 参考: https://github.com/aws/aws-sdk-js/issues/1963#issuecomment-508210959
 // 参考: https://jestjs.io/docs/ja/tutorial-async
+const mockS3PutObject = jest.fn();
 jest.mock('aws-sdk', () => {
     return {
         S3: () => {
             return {
-                putObject: () => {
-                    return {
-                        promise: () => {
-                            return new Promise((resolve) => {
-                                resolve();
-                            })
-                        },
-                    }
-                },
+                putObject: mockS3PutObject,
             }
         },
     };
 });
+
 describe('Tests index', () => {
+    beforeEach(() => {
+        mockS3PutObject.mockReset();
+        process.env.VIMAGEMORE_BUCKET_NAME = 'vimagemore_test_bucket';
+    });
+
     test('verifies successful response', done => {
+        mockS3PutObject.mockImplementation((params) => {
+            return {
+                promise() {
+                    return Promise.resolve();
+                }
+            };
+        });
+
+        const image = fs.readFileSync('./src/__tests__/150x150.png');
+        const event_body = {
+            image: image.toString('base64'),
+        };
+
         const callback:Function = (err:any, result:any) => {
             try {
+                expect(mockS3PutObject.mock.calls).toEqual([
+                    [{
+                        Bucket: 'vimagemore_test_bucket',
+                        Key: expect.stringMatching(/^images\/[\w\-]+?\.png$/),
+                        ContentType: 'image/png',
+                        Body: image,
+                    }]
+                ]);
                 expect(result).toEqual(expect.any(Object));
                 expect(result.statusCode).toBe(200);
                 expect(result.body).toEqual(expect.any(String));
@@ -40,6 +61,8 @@ describe('Tests index', () => {
             }
         };
 
-        app.lambdaHandler({}, {}, callback);
+        app.lambdaHandler({
+            body: JSON.stringify(event_body),
+        }, {}, callback);
     });
 });
--- uploader/src/app.ts
+++ uploader/src/app.ts
@@ -1,38 +1,49 @@
 import AWS from 'aws-sdk';
+import FileType from 'file-type';
+import uuid from 'uuid';
 
 export function lambdaHandler (event:any, context:any, callback:Function) {
+    const json = JSON.parse(event.body);
+
     /* 画像データをS3へ配置する */
     // 参考: https://docs.aws.amazon.com/ja_jp/sdk-for-javascript/v2/developer-guide/using-promises.html
     // 参考: https://qiita.com/niusounds/items/9be50e9d8538db052275
     // 参考: https://github.com/shimobayashi/vimage/blob/master/vimage.rb#L59
-    //XXX event.bodyにJSON文字列がPOSTされてきてそいつをparseして処理する感じにすると楽？
     const s3 = new AWS.S3();
-    const params:AWS.S3.Types.PutObjectRequest = {
-        Bucket: process.env.VIMAGEMORE_BUCKET_NAME || '',
-        Key: 'image/test.txt', //XXX なんかいい感じに決める
-        ContentType: 'text/plain', //XXX https://dev.classmethod.jp/server-side/node-js-server-side/node-js-get-file-extension-and-mime-type/
-        Body: 'this is test', //XXX busboyを使って画像を読み込む→なんかめんどそうなのでJSONをPOSTされる想定にすると良い気がしてきた
-    };
-    s3.putObject(params).promise().then((data) => {
-        console.log('Success', data);
+    const image = Buffer.from(json.image, 'base64');
+    FileType.fromBuffer(image).then((filetype) => {
+        if (filetype === undefined) {
+            throw new Error('filetype is not detected');
+        }
 
-        /* メタデータをDynamoDBへ記録する */
-        //XXX
+        //XXX 投稿した画像はpublic-readにする
+        const params:AWS.S3.Types.PutObjectRequest = {
+            Bucket: process.env.VIMAGEMORE_BUCKET_NAME || '',
+            Key: `images/${uuid.v4()}.${filetype.ext}`,
+            ContentType: filetype.mime,
+            Body: image,
+        };
+        s3.putObject(params).promise().then((data) => {
+            console.log('Success', data);
 
-        callback(null, {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'Succeed!',
-            })
+            /* メタデータをDynamoDBへ記録する */
+            //XXX
+
+            callback(null, {
+                'statusCode': 200,
+                'body': JSON.stringify({
+                    message: 'Succeed!',
+                })
+            });
+        }).catch((err) => {
+            console.log('Error', err);
+
+            callback(err, {
+                'statusCode': 500,
+                'body': JSON.stringify({
+                    message: err,
+                })
+            });
         });
-    }).catch((err) => {
-        console.log('Error', err);
-
-        callback(err, {
-            'statusCode': 500,
-            'body': JSON.stringify({
-                message: err,
-            })
-        });
-    })
+    });
 }
