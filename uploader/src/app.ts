0faae4c85838a2bacbd2054e4a3e8a802f7a086a
shimobayashi <akimasashimobayashi@gmail.com>
2020-02-16 21:19:03 +0900

    もっとマシな書き方に変換した。

--- uploader/src/app.ts
+++ uploader/src/app.ts
@@ -8,31 +8,26 @@ export async function lambdaHandler (event:any) {
     // 参考: https://github.com/shimobayashi/vimage/blob/master/vimage.rb#L59
     const json = JSON.parse(event.body);
     const image = Buffer.from(json.image, 'base64');
-    const promise = new Promise((resolve, reject) => {
-        FileType.fromBuffer(image).then((filetype) => {
-            if (filetype === undefined) {
-                throw new Error('filetype is not detected');
-            }
+    return FileType.fromBuffer(image).then((filetype) => {
+        if (filetype === undefined) {
+            throw new Error('filetype is not detected');
+        }
 
-            const params:AWS.S3.Types.PutObjectRequest = {
-                Bucket: process.env.VIMAGEMORE_BUCKET_NAME || '',
-                Key: `images/${uuid.v4()}.${filetype.ext}`,
-                ContentType: filetype.mime,
-                Body: image,
-                ACL: 'public-read',
-            };
-            const s3 = new AWS.S3();
-            return s3.putObject(params).promise();
-        }).then((data) => {
-            /* メタデータをDynamoDBへ記録する */
-            //XXX
+        const params:AWS.S3.Types.PutObjectRequest = {
+            Bucket: process.env.VIMAGEMORE_BUCKET_NAME || '',
+            Key: `images/${uuid.v4()}.${filetype.ext}`,
+            ContentType: filetype.mime,
+            Body: image,
+            ACL: 'public-read',
+        };
+        const s3 = new AWS.S3();
+        return s3.putObject(params).promise();
+    }).then((data) => {
+        /* メタデータをDynamoDBへ記録する */
+        //XXX
 
-            resolve({
-                statusCode: 200
-            });
-        }).catch((error) => {
-            reject(error);
-        });
+        return {
+            statusCode: 200,
+        };
     });
-    return promise;
 }
