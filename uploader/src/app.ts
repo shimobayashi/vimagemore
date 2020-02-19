c299c8fde096cdff983e1cf039c7d059b3f3031d
shimobayashi <akimasashimobayashi@gmail.com>
2020-02-19 20:22:21 +0900

    重複したkeyを指定しているときに無駄な情報のやり取りをしないで済むように処理の順序を変えた。

--- uploader/src/app.ts
+++ uploader/src/app.ts
@@ -7,26 +7,16 @@ export async function lambdaHandler (event:any) {
         throw new Error('json.key is not found');
     }
     const image = Buffer.from(json.image, 'base64');
+
     var key = '';
+    var contentType = '';
     return FileType.fromBuffer(image).then((filetype) => {
         if (filetype === undefined) {
             throw new Error('filetype is not detected');
         }
-
-        /* 画像データをS3へ配置する */
-        // 参考: https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/nodejs-prog-model-handler.html#nodejs-handler-async
-        // 参考: https://github.com/shimobayashi/vimage/blob/master/vimage.rb#L59
         key = `images/${json.key}.${filetype.ext}`;
-        const params:AWS.S3.Types.PutObjectRequest = {
-            Bucket: process.env.VIMAGEMORE_BUCKET_NAME ?? '',
-            Key: key,
-            ContentType: filetype.mime,
-            Body: image,
-            ACL: 'public-read',
-        };
-        const s3 = new AWS.S3();
-        return s3.putObject(params).promise();
-    }).then(() => {
+        contentType = filetype.mime;
+
         /* メタデータをDynamoDBへ記録する */
         // 参考: https://docs.aws.amazon.com/ja_jp/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.03.html#GettingStarted.NodeJs.03.01
         const docClient = new AWS.DynamoDB.DocumentClient();
@@ -39,13 +29,25 @@ export async function lambdaHandler (event:any) {
                 CreatedAt: epoch,
                 UpdatedAt: epoch,
             },
-            //XXX DynamoDBへの記録を先にやって失敗させた方が無駄なS3へのputが減って良さそう
             Expected: {
                 Key: {
                     Exists: false,
                 },
             },
         }).promise();
+    }).then(() => {
+        /* 画像データをS3へ配置する */
+        // 参考: https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/nodejs-prog-model-handler.html#nodejs-handler-async
+        // 参考: https://github.com/shimobayashi/vimage/blob/master/vimage.rb#L59
+        const params:AWS.S3.Types.PutObjectRequest = {
+            Bucket: process.env.VIMAGEMORE_BUCKET_NAME ?? '',
+            Key: key,
+            ContentType: contentType,
+            Body: image,
+            ACL: 'public-read',
+        };
+        const s3 = new AWS.S3();
+        return s3.putObject(params).promise();
     }).then(() => {
         return {
             statusCode: 200,
