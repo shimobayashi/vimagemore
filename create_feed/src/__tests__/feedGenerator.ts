f57042cdc76bd15df4863c70b062a3bbc37c2c15
shimobayashi <akimasashimobayashi@gmail.com>
2020-03-01 20:29:27 +0900

    とりあえず空のテストを書いた。

---
+++ create_feed/src/__tests__/feedGenerator.ts
@@ -0,0 +1,26 @@
+import * as FeedGenerator from '../feedGenerator';
+
+test('フィードを期待通りに生成できる', () => {
+    const feed = FeedGenerator.generateFeed({
+        Id: 'test tag 1',
+    }, [
+        {
+            Id: 'test image 2',
+            Path: 'images/test_image_2.png',
+            Title: 'test image 2',
+            Tags: ['test tag 1'],
+            CreatedAt: 1482363367,
+            UpdatedAt: 1482363367,
+        },
+        {
+            Id: 'test image 1',
+            Path: 'images/test_image_1.png',
+            Title: 'test image 1',
+            Tags: ['test tag 1'],
+            CreatedAt: 1482363367,
+            UpdatedAt: 1482363367,
+        },
+    ]);
+
+    expect(feed).toEqual('<xml></xml>');
+});
--- create_feed/src/feedGenerator.ts
+++ create_feed/src/feedGenerator.ts
@@ -1,8 +1,5 @@
 import AWS from 'aws-sdk';
 
 export function generateFeed(imageTag:AWS.DynamoDB.DocumentClient.AttributeMap, images:AWS.DynamoDB.DocumentClient.ItemList) {
-    console.log(imageTag.Id);
-    console.log(images);
-
     return '<xml></xml>';
 }
