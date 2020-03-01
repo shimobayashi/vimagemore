33486a23d82a9b0511198a25382a16d558cd3905
shimobayashi <akimasashimobayashi@gmail.com>
2020-03-01 22:42:52 +0900

    とりあえず必要な実装はしたはずだが、本番で動かしてないので動くのかどうかは知らない。

--- create_feed/src/feedGenerator.ts
+++ create_feed/src/feedGenerator.ts
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk';
 import { Feed } from 'feed';
 
-export function generateFeed(imageTag:AWS.DynamoDB.DocumentClient.AttributeMap, images:AWS.DynamoDB.DocumentClient.ItemList) {
+export function generateFeed(websiteUrl:string, imageTag:AWS.DynamoDB.DocumentClient.AttributeMap, images:AWS.DynamoDB.DocumentClient.ItemList) {
     const feed = new Feed({
         title: `"${imageTag.Id}" images - vimagemore`,
         description: `Images tagged "${imageTag.Id}"`,
@@ -15,7 +15,7 @@ export function generateFeed(imageTag:AWS.DynamoDB.DocumentClient.AttributeMap, 
             title: image.Title,
             id: `${image.Id}@vimagemore`,
             link: image.Link,
-            description: imageToDescription(image),
+            description: imageToDescription(websiteUrl, image),
             date: new Date(image.UpdatedAt * 1000),
             // content要素を設定しても良いかも知れない
             // image要素を設定しても良いかも知れない
@@ -25,9 +25,9 @@ export function generateFeed(imageTag:AWS.DynamoDB.DocumentClient.AttributeMap, 
     return feed.rss2();
 }
 
-function imageToDescription(image:AWS.DynamoDB.DocumentClient.AttributeMap) {
+function imageToDescription(websiteUrl:string, image:AWS.DynamoDB.DocumentClient.AttributeMap) {
     const tagHtml = `<div class="tags">${image.Tags.join(', ')}</div>`;
-    const src = ''; //XXX
+    const src = websiteUrl + image.Path;
     // 本来ならinline-styleでmax-height: 600pxなどと指定したいが、
     // Feedlyはinline-styleを解釈しないようなので指定していない
     // 参考: https://feedly.uservoice.com/forums/192636-suggestions/suggestions/4124796
