838e5780b50280ae9818bb3b7b0190c8fb2e22a7
shimobayashi <akimasashimobayashi@gmail.com>
2020-04-06 19:52:16 +0900

    画像を削除するバッチの雛形をとりあえず書いた。

---
+++ expire_image/src/__tests__/app.ts
@@ -0,0 +1,4 @@
+import * as app from '../app';
+
+describe('Tests index', () => {
+});
---
+++ expire_image/src/app.ts
@@ -0,0 +1,8 @@
+import AWS from 'aws-sdk';
+
+export async function lambdaHandler (event:any) {
+    // 古くなっているImageを取得する
+    // 古くなっているImageのImageTagを消す
+    // 画像データの実体を消す
+    // Image自身を消す
+}
